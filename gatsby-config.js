40b7267c7f9442adb3c992bac0b8e898479452c8
Zack <zacharybryant95@gmail.com>
2024-07-11 13:46:50 -0700

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'// Step 2: Define your component
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/icon.png"
+      />
     </Layout>
   )
 }
